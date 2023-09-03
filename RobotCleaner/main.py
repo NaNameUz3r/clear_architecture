2f525feef4369791e090844f43658952d8ecbf7d
Ivan Zakutnii <zakutnii.ivan@gmail.com>
2023-09-03 16:30:38 +0400

    task_8

--- RobotCleaner/main.py
+++ RobotCleaner/main.py
@@ -1,17 +1,15 @@
-from typing import NoReturn
 from package.robot_cleaner import RobotCleaner
 from controllers.input_controller import InputReconciler
 from dsl_interpreter.cleaner_interpreter import CleanerInterpreter
 from controllers.cleaner_controller import CleanerController
 
 
-def main() -> NoReturn:
+def main():
     action_controller = CleanerController()
     interpreter = CleanerInterpreter()
-    robot = RobotCleaner(dsl_interpreter=interpreter,
-                         action_controller=action_controller)
-    print(type(robot))
-    print(robot)
+    robot = RobotCleaner(
+        dsl_interpreter=interpreter, action_controller=action_controller
+    )
     reconciler = InputReconciler(robot_to_control=robot)
     reconciler.start_loop()
 
