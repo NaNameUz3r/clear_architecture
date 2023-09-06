b33bd8cac8bb007f2e4137bb8a7323e0d4f07da3
Ivan Zakutnii <zakutnii.ivan@gmail.com>
2023-09-06 21:44:25 +0400

    task_10

--- Architecure/concatenate_api/client_side.py
+++ Architecure/concatenate_api/client_side.py
@@ -1,4 +1,4 @@
 import server_side
 
 api = server_side.ServerApi()
-s = api("100 move -90 turn soap set start 50 move stop 20 turn brush set start stop")
+print(api("100 move -90 turn soap set start 50 move stop 20 turn brush set start stop"))
